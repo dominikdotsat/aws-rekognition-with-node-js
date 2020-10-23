9b88afba3deb6469bbffe532f24c951d816939ee
Dominik Sz <dominik.szathmary@onefootball.com>
2020-10-23 11:07:14 +0200

    chore: add cors + multiface support

--- resources/aws-rekogniton/facial-detection.js
+++ resources/aws-rekogniton/facial-detection.js
@@ -16,48 +16,48 @@ var objReturn = {
 }
 
 module.exports.search_face = function(obj, callback){
-	
+
 	console.log("Find face ...")
-	
+
 	rekognition.searchFacesByImage({
 	 	"CollectionId": config.collectionName,
-	 	"FaceMatchThreshold": 70, //set minumum match in image send
+	 	"FaceMatchThreshold": 60, //set minumum match in image send
 	 	"Image": {
 			'Bytes': new Buffer(obj.photo, 'base64')
 	 	},
-	 	"MaxFaces": 1 // set the number face detect in image send
+	 	"MaxFaces": 100 // set the number face detect in image send
 	}, function(err, data) {
 	 	if (err) {
 			console.log(err)
 			callback(err);
-	 	} else { 
+	 	} else {
 			if(data.FaceMatches && data.FaceMatches.length > 0 && data.FaceMatches[0].Face)
 			{
 				console.log(data.FaceMatches[0].Face)
-				callback (data.FaceMatches[0].Face);	
+				callback (data.FaceMatches.map((face)=>face.Face));
 			} else {
 				objReturn.found = false
-				callback (objReturn);	
+				callback (objReturn);
 			}
 		}
 	});
 }
 
 module.exports.indexFaces = function (obj,callback){
-	
+
 	console.log("Index new image face ...")
 
 	rekognition.indexFaces({
 		"CollectionId": config.collectionName,
 		"DetectionAttributes": [ "ALL" ], // set detect all atributes on image send.
-		"ExternalImageId": obj.id_user, 
-		"Image": { 
+		"ExternalImageId": obj.id_user,
+		"Image": {
 			'Bytes': new Buffer(obj.photo, 'base64')
 		}
 	}, function(err, data) {
 		if (err) {
 			console.log(err, err.stack); // an error occurred
-			
+
 			objReturn.found = false
 			objReturn.resultAWS = err, err.stack
 			callback(objReturn)
@@ -75,7 +75,7 @@ module.exports.indexFaces = function (obj,callback){
 module.exports.deleteFace = function (obj,callback){
 
 	console.log("Delete image face ...")
-	
+
 	//prepare params to delete face index.
 	var params_deletion = {CollectionId: config.collectionName, FaceIds: [obj.face_id]};
 
--- resources/config/aws-config.js
+++ resources/config/aws-config.js
@@ -1,2 +1,7 @@
 module.exports.collectionName = "PUT YOUR COLLECTION NAME HERE";
 module.exports.region = "PUT YOUR REGION COLLECTION HERE";
+/*
+*  FOR OF demo put:
+*   module.exports.collectionName = "champions_league_players";
+*   module.exports.region = "eu-west-1";
+* */
